db7de8d60411d667804e0428fe8a63170f2a02f8
Nick Elsey <nick@spelloconsulting.com>
2026-05-01 17:34:06 +1000

    Fix exports

--- src/sc_foundation/__init__.py
+++ src/sc_foundation/__init__.py
@@ -9,6 +9,7 @@ from .sc_csv_reader import CSVReader
 from .sc_date_helper import DateHelper
 from .sc_json_encoder import JSONEncoder
 from .sc_logging import SCLogger
+from .thread_manager import ManagedThread, RestartPolicy, ThreadManager
 from .validation_schema import yaml_config_validation
 
-__all__ = ["CSVReader", "DateHelper", "JSONEncoder", "SCCommon", "SCConfigManager", "SCLogger", "yaml_config_validation"]
+__all__ = ["CSVReader", "DateHelper", "JSONEncoder", "ManagedThread", "RestartPolicy", "SCCommon", "SCConfigManager", "SCLogger", "ThreadManager", "yaml_config_validation"]
